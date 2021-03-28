e92a0636727a6f5bf9454fdc5df9fe4a2aaa1e6d
macdrien <adrien.bouyssou@gmail.com>
2021-03-28 10:59:36 +0200

    [REF] Move header css into a separate file

--- src/subpages/Header.js
+++ src/subpages/Header.js
@@ -1,13 +1,16 @@
 import linkedinLogo from "../images/linkedin-3-32.png";
 import githubLogo from "../images/GitHub-Mark-Light-32px.png";
 
+import './Header.css';
+
 export default function Header() {
     return <header>
         <div className="headerAndLinks">
             <h1>Adrien Bouyssou</h1>
 
             <ul className="links">
-                <li><a href="https://www.linkedin.com/in/adrien-bouyssou-0b326b14b/"><img src={linkedinLogo} alt="Logo LinkedIn"/></a>
+                <li><a href="https://www.linkedin.com/in/adrien-bouyssou-0b326b14b/"><img src={linkedinLogo}
+                                                                                          alt="Logo LinkedIn"/></a>
                 </li>
                 <li>adrien.bouyssou@sidranie.fr</li>
                 <li><a href="https://github.com/macdrien"><img src={githubLogo} alt="Logo GitHub"/></a></li>
@@ -18,12 +21,15 @@ export default function Header() {
             Systèmes Clemessy</em>, la programmation s'est imposée à moi comme ma plus grande passion il y a maintenant
             plus de 5 ans. Investi dans ce que j'entreprends, j'ai traversé la France afin de passer mon diplôme
             d'ingénieur. Ouvert à l'échange et désireux d'en apprendre toujours plus sur un sujet, je suis toujours à la
-            recherche de nouveaux défis qui viendront enrichir mon expérience et affûter mes capacités de programmation et
+            recherche de nouveaux défis qui viendront enrichir mon expérience et affûter mes capacités de programmation
+            et
             d'adaptation.<br/>
-            Ayant toujours étudié le <strong>Java</strong>, à l'école et pendant mon apprentissage, il est aujourd'hui mon
+            Ayant toujours étudié le <strong>Java</strong>, à l'école et pendant mon apprentissage, il est aujourd'hui
+            mon
             langage de prédilection pour construire des APIs HTTP.<br/>
             <strong>Python</strong> m'accompagne également dans la création de scripts ou dans mon perfectionnement de
-            l'algorithmie, en particulier sur <a href="https://www.codewars.com/users/macdrien">CodeWars.com</a> où il m'a
+            l'algorithmie, en particulier sur <a href="https://www.codewars.com/users/macdrien">CodeWars.com</a> où il
+            m'a
             permis d'atteindre le 5ème kyu.
         </p>
     </header>;
