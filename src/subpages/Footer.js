a3ea88f6dd53b3361ec2a70afd7105670a608f06
macdrien <adrien.bouyssou@gmail.com>
2021-03-28 11:02:39 +0200

    [REF] Move footer css into a separate file

--- src/subpages/Footer.js
+++ src/subpages/Footer.js
@@ -1,14 +1,21 @@
 import linkedinLogo from "../images/linkedin-3-32.png";
 import githubLogo from "../images/GitHub-Mark-Light-32px.png";
 
+import './Footer.css';
+
 export default function Footer() {
     return <footer>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <p className="topPage"><a href="#">Haut de page</a></p>
 
         <div>
-            <a href="https://www.linkedin.com/in/adrien-bouyssou-0b326b14b/"><img src={linkedinLogo} alt="Logo LinkedIn"/></a>
-            <a href="https://github.com/macdrien"><img src={githubLogo} alt="Logo Github"/></a>
+            <a href="https://www.linkedin.com/in/adrien-bouyssou-0b326b14b/">
+                <img src={linkedinLogo}
+                     alt="Logo LinkedIn"/>
+            </a>
+            <a href="https://github.com/macdrien">
+                <img src={githubLogo} alt="Logo Github"/>
+            </a>
         </div>
 
         <p className="versionNumber">v2.0.0</p>
