0786d3f6b7d150436acbf6548f5e0acbdb12dc49
macdrien <adrien.bouyssou@gmail.com>
2021-03-28 12:53:12 +0200

    [UPD] Menu fixed on the top and move main page into a separate component

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from "../subpages/Header";
+import data from "../data.json";
+import Section from "../components/Section";
+import Footer from "../subpages/Footer";
+
+export default function Main() {
+    return <React.Fragment><Header/>
+        {
+            data.map((element, index) => <Section {...element} key={index}/>)
+        }
+        <Footer/>
+    </React.Fragment>;
+}
