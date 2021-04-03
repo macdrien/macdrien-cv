8314c91d4acf12ed6cd8dae8e9d45d0054cb89f2
macdrien <adrien.bouyssou@gmail.com>
2021-04-03 22:46:49 +0200

    [ADD] CV modal window with React-Modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import './App.css';
+import React from 'react';
 import {
     Route,
     Router,
     Switch,
 } from 'react-router-dom';
+import ReactModal from 'react-modal';
 import Menu from './subpages/Menu';
 import Main from "./pages/Main";
 import {createBrowserHistory} from 'history';
+import cv from './documents/cv_adrien_bouyssou.pdf';
 
-function App() {
-    return (
-        <Router history={createBrowserHistory()}>
-            <Menu/>
-            <div className="App">
-                <Switch>
-                    <Route path='/'>
-                        <Main/>
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+ReactModal.setAppElement('#root');
+
+class App extends React.Component {
+    state = {
+        isCVModalOpened: false,
+    };
+
+    history = createBrowserHistory();
+
+    closeCVModal = (event) => {
+        this.setState({isCVModalOpened: false});
+    }
+
+    render() {
+        return (
+            <Router history={this.history}>
+                <Menu openCVModal={(event) => this.setState({isCVModalOpened: true})}/>
+                <div className="App">
+                    <Switch>
+                        <Route path='/'>
+                            <Main/>
+                        </Route>
+                    </Switch>
+                    <ReactModal isOpen={this.state.isCVModalOpened}
+                                shouldCloseOnOverlayClick={true}
+                                onRequestClose={this.closeCVModal}
+                                className='cvModal'
+                    >
+                        <iframe className='cv' title='Adrien Bouyssou CV' src={cv} allowFullScreen/>
+                    </ReactModal>
+                </div>
+            </Router>
+        );
+    };
 }
 
 export default App;
--- src/subpages/Menu.js
+++ src/subpages/Menu.js
@@ -1,11 +1,23 @@
-import {Link} from "react-router-dom";
+import React from 'react';
+import {Link} from 'react-router-dom';
+import pdfIcon from '../images/pdf_icon.png';
+import PropTypes from 'prop-types';
 
 import './Menu.css';
 
-export default function Menu() {
-    return <nav>
-        <ul>
-            <li><Link to='/'>Acceuil</Link></li>
-        </ul>
-    </nav>;
+export default class Menu extends React.Component {
+    PropTypes = {
+        openCVModal: PropTypes.func.isRequired,
+    };
+
+    render() {
+        return <nav>
+            <ul>
+                <li><Link to='/'>Acceuil</Link></li>
+            </ul>
+            <div onClick={this.props.openCVModal}>
+                <img src={pdfIcon} alt='PDF icon to my CV'/>
+            </div>
+        </nav>;
+    }
 }
