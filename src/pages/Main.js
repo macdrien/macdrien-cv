64910c4c6554524408790f78a1eb965d99af86be
macdrien <adrien.bouyssou@gmail.com>
2021-04-09 20:34:40 +0200

    [REF] Move data.json into the folder resources

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Header from "../subpages/Header";
-import data from "../data.json";
+import data from "../resources/data.json";
 import Section from "../components/Section";
 import Footer from "../subpages/Footer";
 
